d056803ed27125236abfee4961e5dd4a98667344
rm-netsu <netsu.code@gmail.com>
2024-06-10 23:36:43 +0300

    remove namespace re-parsing

--- src/generic/root.ts
+++ src/generic/root.ts
@@ -2,14 +2,12 @@ import { NAMESPACES, RootNode, TagmlNode } from '../node/index'
 import { GenericHeadNode, useGenericHeadNode } from './root/head'
 
 export class GenericRootNode extends RootNode {
-	namespace: string
 	head: GenericHeadNode | null = null
 	body: TagmlNode | null = null
 
 	constructor(node: RootNode) {
 		super(node)
-		this.namespace = this.attributes.get('namespace') ?? 'generic'
-
+		
 		useGenericHeadNode(this)
 		this.body = this.query.child($ => $.nodeName === 'body')
 	}
