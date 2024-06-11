9bc0edd170e07058650c22319d6c023fe9e9893f
rm-netsu <netsu.code@gmail.com>
2024-06-11 14:27:50 +0300

    allow store entity ref in metadata

--- src/generic/root/head/import/entity.ts
+++ src/generic/root/head/import/entity.ts
@@ -11,14 +11,19 @@ export class EntityImportNode extends TagmlNode {
 		super(node)
 		this.packageName = this.requireString('from')
 		this.entityType = this.nodeName
-		this.entityName = this.requireString('ref')
+		const entityName = this.attributes.get('ref') ?? this.rawNodeMetadata
+		if(!entityName) throw new Error('Entity import must contain ref')
+		
+		this.entityName = entityName
 		this.entityAlias = this.attributes.get('as') ?? null
 	}
 }
 
 export const useEntityImportNode = (importNode: GenericImportNode) => {
 	const entityImport = importNode.query.childAs(
-		$ => $.hasAttributes('ref', 'from'),
+		$ =>
+			($.attributes.has('ref') || $.rawNodeMetadata !== null)
+			&& $.attributes.has('from'),
 		EntityImportNode
 	)
 	if(entityImport) {
